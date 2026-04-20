83b3b991e9b57f4bf492768207dcce28bc3a9c8c
Vishvam2006 <vishvam.r.modi@gmail.com>
2026-04-20 11:10:13 +0530

    kanpsack

--- Practical_9/0_1_knapsack.cpp
+++ Practical_9/0_1_knapsack.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 struct Item {
-    int w, p;
-    double r;
+    int weight, profit;
+    double ratio;
 };
 
 struct Node {
@@ -15,7 +15,81 @@ struct Node {
 };
 
 bool cmp(Item a, Item b) {
-    return a.r > b.r;
+    return a.ratio > b.ratio;
 }
 
+double bound(Node u, int n, int W, vector<Item>& items) {
+    if (u.weight >= W) return 0;
 
+    double profitBound = u.profit;
+    int j = u.level + 1;
+    int totalWeight = u.weight;
+
+    while (j < n && totalWeight + items[j].weight <= W) {
+        totalWeight += items[j].weight;
+        profitBound += items[j].profit;
+        j++;
+    }
+
+    if (j < n)
+        profitBound += (W - totalWeight) * items[j].ratio;
+
+    return profitBound;
+}
+
+int knapsack(int W, vector<Item>& items, int n) {
+    sort(items.begin(), items.end(), cmp);
+
+    priority_queue<pair<double, Node>> pq;
+
+    Node u, v;
+    u.level = -1;
+    u.profit = 0;
+    u.weight = 0;
+    u.bound = bound(u, n, W, items);
+
+    pq.push({u.bound, u});
+
+    int maxProfit = 0;
+
+    while (!pq.empty()) {
+        u = pq.top().second;
+        pq.pop();
+
+        if (u.bound <= maxProfit) continue;
+
+        v.level = u.level + 1;
+
+        v.weight = u.weight + items[v.level].weight;
+        v.profit = u.profit + items[v.level].profit;
+
+        if (v.weight <= W && v.profit > maxProfit)
+            maxProfit = v.profit;
+
+        v.bound = bound(v, n, W, items);
+        if (v.bound > maxProfit)
+            pq.push({v.bound, v});
+
+        v.weight = u.weight;
+        v.profit = u.profit;
+
+        v.bound = bound(v, n, W, items);
+        if (v.bound > maxProfit)
+            pq.push({v.bound, v});
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int W = 16;
+
+    vector<Item> items = {
+        {2, 40, 40.0/2},
+        {5, 30, 30.0/5},
+        {10, 50, 50.0/10},
+        {5, 10, 10.0/5}
+    };
+
+    cout << knapsack(W, items, items.size());
+}
